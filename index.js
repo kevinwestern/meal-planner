39225ab06aa74425b078ee9d217be198954788ed
kevinwestern <kevinwestern@gmail.com>
2016-03-16 21:45:54 -0700

    Moved some logic to the store.

--- index.js
+++ index.js
@@ -5,10 +5,112 @@ import { Router, Route, hashHistory } from 'react-router'
 import MealApp from './src/app'
 import WeekPlan from './src/week-plan'
 
+function foodApp(state = initialState, action) {
+  // For now, don’t handle any actions
+  // and just return the state given to us.
+  return state;
+};
+
+let meals = localStorage.getItem('meals');
+if (!meals) {
+  meals = [];
+} else {
+  meals = JSON.parse(meals);
+}
+
+// let store = createStore(foodApp, foods);
+let newMealForm = [{
+  id: 'name',
+  label: 'Name:',
+  name: 'name',
+  placeholder: 'Baked Chicken',
+  required: true,
+  type: 'text',
+  value: '',
+}, {
+  id: 'cals',
+  label: 'Calories:',
+  name: 'cals',
+  placeholder: 450,
+  required: false,
+  type: 'number',
+  value: '',
+}, {
+  id: 'protein',
+  label: 'Protein:',
+  name: 'protein',
+  placeholder: 50,
+  required: false,
+  type: 'number',
+  value: '',
+}, {
+  id: 'carbs',
+  label: 'Carbs:',
+  name: 'carbs',
+  placeholder: 40,
+  required: false,
+  type: 'number',
+  value: '',
+}, {
+  id: 'fat',
+  label: 'Fat:',
+  name: 'fat',
+  placeholder: 10,
+  required: false,
+  type: 'number',
+  value: '',
+}];
+
+const store = {
+  listeners_: [],
+  newMealForm: newMealForm,
+  meals: meals,
+
+  updateFormField: function (index, value) {
+    this.newMealForm = this.newMealForm.map((field, i) => {
+      return i == index ?
+        Object.assign({}, field, {value}) :
+        field;
+    })
+    this.dispatchChange();
+  },
+
+  addMeal: function(mealForm) {
+    this.meals.push(this.formToJson_(mealForm));
+    localStorage.setItem('meals', JSON.stringify(this.meals));
+    this.newMealForm = newMealForm;
+    this.dispatchChange();
+  },
+
+  formToJson_: function(form) {
+    return {
+      name: form[0].value,
+      nutrients: form.slice(1).map(nutrient => ({name: nutrient.name, value: nutrient.value}))
+    };
+  },
+
+  addChangeListener: function(listener) {
+    this.listeners_.push(listener);
+  },
+
+  getNewMealForm: function() {
+    return this.newMealForm;
+  },
+
+  getMeals: function() {
+    return this.meals;
+  },
+
+  dispatchChange: function() {
+    this.listeners_.forEach(listener => {
+      listener();
+    });
+  }
+}
+
 render(
-  <Router history={hashHistory}>
-    <Route path="/" component={MealApp}/>
-    <Route path="/week" component={WeekPlan}/>
-  </Router>,
+  <MealApp meals={meals}
+           newMealForm={newMealForm}
+           store={store}></MealApp>,
   document.getElementById('content')
 );
--- src/app.js
+++ src/app.js
@@ -7,80 +7,29 @@ import FOODS from '../seed-data'
 export default class MealApp extends Component {
   constructor(props, context) {
     super(props, context);
-    const foods = localStorage.getItem('foods');
     this.state = {
-      compactMeals: foods ? JSON.parse(foods) : FOODS,
-      newMealForm: [{
-        id: 'name',
-        label: 'Name:',
-        name: 'name',
-        placeholder: 'Baked Chicken',
-        required: true,
-        type: 'text',
-        value: '',
-      }, {
-        id: 'cals',
-        label: 'Calories:',
-        name: 'cals',
-        placeholder: 450,
-        required: false,
-        type: 'number',
-        value: '',
-      }, {
-        id: 'protein',
-        label: 'Protein:',
-        name: 'protein',
-        placeholder: 50,
-        required: false,
-        type: 'number',
-        value: '',
-      }, {
-        id: 'carbs',
-        label: 'Carbs:',
-        name: 'carbs',
-        placeholder: 40,
-        required: false,
-        type: 'number',
-        value: '',
-      }, {
-        id: 'fat',
-        label: 'Fat:',
-        name: 'fat',
-        placeholder: 10,
-        required: false,
-        type: 'number',
-        value: '',
-      }]
+      newMealForm: this.props.store.getNewMealForm(),
+      meals: this.props.store.getMeals()
     };
+  }
+
+  componentDidMount() {
+    this.props.store.addChangeListener(this.updateState_.bind(this))
   }
 
   addFood(e) {
     e.preventDefault();
-    const foods = JSON.parse(localStorage.getItem('foods') || '[]');
-    foods.push(this.formToJson_(this.state.newMealForm));
-    localStorage.setItem('foods', JSON.stringify(foods));
-    this.setState({
-      compactMeals: foods,
-      newMealForm: this.state.newMealForm.map(field => {
-        return Object.assign({}, field, {value: ''});
-      })
-    });
-  }
-
-  formToJson_(newMealForm) {
-    return {
-      name: newMealForm[0].value,
-      nutrients: newMealForm.slice(1).map(nutrient => ({name: nutrient.name, value: nutrient.value}))
-    };
+    this.props.store.addMeal(this.state.newMealForm);
   }
 
   updateFormState(e) {
+    this.props.store.updateFormField(e.target.dataset.index, e.target.value)
+  }
+
+  updateState_() {
     this.setState({
-      newMealForm: this.state.newMealForm.map((field, index) => {
-        return index == e.target.dataset.index ?
-          Object.assign({}, field, {value: e.target.value}) :
-          field;
-      })
+      newMealForm: this.props.store.getNewMealForm(),
+      meals: this.props.store.getMeals()
     });
   }
 
@@ -89,7 +38,7 @@ export default class MealApp extends Component {
       <div>
         <h1>Meals</h1>
         <ul>
-          {this.state.compactMeals.map(meal => {
+          {this.state.meals.map(meal => {
             return <li key={meal.name}><CompactMeal meal={meal}></CompactMeal></li>
           })}
         </ul>
