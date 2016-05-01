f4a745da1e9acc28e7e13bc82a8b53023ce8209e
kevinwestern <kevinwestern@gmail.com>
2016-04-30 18:44:20 -0700

    Added bottom tab.

--- src/edit-meal.js
+++ src/edit-meal.js
@@ -17,39 +17,45 @@ export default class EditMeal extends Component {
     this.props.store.saveEditMealForm(this.props.form);
   }
 
+  delete() {
+
+  }
+
   render() {
     return (
       <form onSubmit={this.submit.bind(this)}>
-        <div className="field">
-          <label htmlFor="edit-meal-name">Name:</label>
-          <input type="text" id="edit-meal-name" value={this.props.form.name}
+        <div className="mdl-textfield mdl-js-textfield">
+          <label htmlFor="edit-meal-name">Name</label>
+          <input className="mdl-textfield__input" type="text" id="edit-meal-name" value={this.props.form.name}
                  onChange={this.update('name')}/>
         </div>
 
-        <div className="field">
+        <div className="mdl-textfield mdl-js-textfield">
           <label htmlFor="edit-meal-calories">Calories:</label>
-          <input type="number" id="edit-meal-calories" value={this.props.form.nutrients.calories}
+          <input className="mdl-textfield__input" type="number" id="edit-meal-calories" value={this.props.form.nutrients.calories}
                  onChange={this.update('calories')}/>
         </div>
 
-        <div className="field">
+        <div className="mdl-textfield mdl-js-textfield">
           <label htmlFor="edit-meal-carbs">Carbs:</label>
-          <input type="number" id="edit-meal-carbs" value={this.props.form.nutrients.carbs}
+          <input className="mdl-textfield__input" type="number" id="edit-meal-carbs" value={this.props.form.nutrients.carbs}
                  onChange={this.update('carbs')}/>
         </div>
 
-        <div className="field">
+        <div className="mdl-textfield mdl-js-textfield">
           <label htmlFor="edit-meal-protein">Protein:</label>
-          <input type="number" id="edit-meal-protein" value={this.props.form.nutrients.protein}
+          <input className="mdl-textfield__input" type="number" id="edit-meal-protein" value={this.props.form.nutrients.protein}
                  onChange={this.update('protein')}/>
         </div>
 
-        <div className="field">
+        <div className="mdl-textfield mdl-js-textfield">
           <label htmlFor="edit-meal-fat">Fat:</label>
-          <input type="number" id="edit-meal-fat" value={this.props.form.nutrients.fat}
+          <input className="mdl-textfield__input" type="number" id="edit-meal-fat" value={this.props.form.nutrients.fat}
                  onChange={this.update('fat')}/>
         </div>
-        <button type="submit">Save</button>
+
+        <button className="mdl-button mdl-js-button mdl-button--primary" type="submit">Save</button>
+        <button className="mdl-button mdl-js-button mdl-button--accent" onClick={this.delete}>Delete</button>
       </form>
     );
   }
